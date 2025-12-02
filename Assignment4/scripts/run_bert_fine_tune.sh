85d2d40a0f018392330fb625ac1bc663f3bf3d04
audrey352 <aberniercan@gmail.com>
2025-12-01 23:13:44 -0500

    tuning bert on batch sizes --  trying to send an array job on mimi

--- Assignment4/scripts/run_bert_fine_tune.sh
+++ Assignment4/scripts/run_bert_fine_tune.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 #
 #SBATCH -w gpu-teach-01
-#SBATCH -c 4 # number of cores
-#SBATCH --mem=4G  # memory per core 
-#SBATCH --gpus=1
-#SBATCH -t 0-4:00 # time (D-HH:MM)                          # MAX TIME IS 4 HOURS
-#SBATCH -o job_logs/scripts/slurm.%N.%j.out # STDOUT        # LOCATION TO SAVE STDOUT LOGS **IN YOUR OWN MIMI ACCOUNT**
-#SBATCH -e job_logs/scripts/slurm.%N.%j.err # STDERR        # LOCATION TO SAVE STDERR LOGS **IN YOUR OWN MIMI ACCOUNT**
-#SBATCH --mail-user=audreanne.bernier@mail.mcgill.ca        # PUT YOUR EMAIL HERE
+#SBATCH -c 4                     # number of CPU cores
+#SBATCH --mem=4G                 # memory per core
+#SBATCH --gpus=1                 # request 1 GPU per job
+#SBATCH -t 0-4:00                # max time 4 hours
+#SBATCH -o job_logs/scripts/slurm.%N.%j.out   # STDOUT log
+#SBATCH -e job_logs/scripts/slurm.%N.%j.err   # STDERR log
+#SBATCH --mail-user=audreanne.bernier@mail.mcgill.ca
 #SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --array=0-2              # <<< RUN 3 JOBS (batch sizes: 4, 8, 16)
 
-module load miniconda/miniconda-fall2024  # LOAD THE MINICONDA MODULE (has all the packages we need alrrady installed)
+module load miniconda/miniconda-fall2024
 
-python /home/2024/aberni24/Courses/COMP551/Assignment4/scripts/bert_fine_tune.py  /home/2024/aberni24/Courses/COMP551/Assignment4/
+# Define batch sizes for array jobs
+BATCH_SIZES=(4 8 16)
+
+# Select batch size based on SLURM_ARRAY_TASK_ID
+BS=${BATCH_SIZES[$SLURM_ARRAY_TASK_ID]}
+
+echo "Running BERT fine-tuning with batch size: $BS"
+echo "SLURM Job ID: $SLURM_JOB_ID"
+echo "GPU Assigned: $CUDA_VISIBLE_DEVICES"
+
+python /home/2024/aberni24/Courses/COMP551/Assignment4/scripts/bert_fine_tune.py \
+       --batch_size $BS \
+       /home/2024/aberni24/Courses/COMP551/Assignment4/
